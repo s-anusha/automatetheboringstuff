ad36f3b06549361fdc378c7edd0656368fec802e
s-anusha <anusha16212@gmail.com>
2019-06-08 13:35:08 +0530

    Chapter 9: deletingUnneededFiles.py

--- Chapter 9/deletingUnneededFiles.py
+++ Chapter 9/deletingUnneededFiles.py
@@ -13,15 +13,17 @@ if len(sys.argv) < 2:
 	sys.exit()
 
 size = sys.argv[1]
+size = int (size)
 
 print('Enter folder path: ')
 sourceFolder = raw_input()
 print('\n')
 
+print('Files to delete:')
 for folderName, subfolders, filenames in os.walk(sourceFolder):
 	for filename in filenames:
-		filepath = os.path.join(folderName,filename)
-		print(filepath)
-		print(os.path.getsize(filepath)
-		#if os.path.getsize(filepath) > size:
-		#	print(os.path.abspath(filename))
+		filepath = os.path.join(folderName, filename)
+		fileSize = os.path.getsize(filepath)
+		if fileSize > size:
+			print(filepath)
+
